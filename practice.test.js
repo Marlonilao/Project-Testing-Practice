fa46d6cc6ea705ba6561821ff5c42e548fcee417
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-10-06 20:12:04 +0800

    feat: add test for calculator object and add a calculator object with functions for basic math operations

--- practice.test.js
+++ practice.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./index";
+import { capitalize, reverseString, calculator } from "./index";
 
 test("capitalize should exist", () => {
   expect(capitalize).toBeDefined();
@@ -30,4 +30,35 @@ test("takes a string and returns it reversed", () => {
   testCase.forEach((element) => {
     expect(reverseString(element.string)).toBe(element.expected);
   });
+});
+
+test("calculator object with function for add,subtract,divide, and multiply should exist", () => {
+  expect(calculator).toBeDefined();
+  expect(calculator.add).toBeDefined();
+  expect(calculator.subtract).toBeDefined();
+  expect(calculator.divide).toBeDefined();
+  expect(calculator.multiply).toBeDefined();
+});
+
+test("calculator.add, a function that takes two numbers and return the sum of it", () => {
+  expect(calculator.add(1, 1)).toBe(2);
+  expect(calculator.add(2, 3)).toBe(5);
+  expect(calculator.add(0, 0)).toBe(0);
+});
+
+test("calculator.subtract, a function that takes two numbers and returns their difference", () => {
+  expect(calculator.subtract(2, 1)).toBe(1);
+  expect(calculator.subtract(0, 1)).toBe(-1);
+});
+
+test("calculator.divide, a function that takes two numbers and returns their qoutient", () => {
+  expect(calculator.divide(2, 2)).toBe(1);
+  expect(calculator.divide(8, 4)).toBe(2);
+  expect(calculator.divide(100, 5)).toBe(20);
+});
+
+test("calculator.multiply, a funciton that takes 2 numbers and returns their product", () => {
+  expect(calculator.multiply(12, 12)).toBe(144);
+  expect(calculator.multiply(1, 0)).toBe(0);
+  expect(calculator.multiply(2, 2)).toBe(4);
 });
